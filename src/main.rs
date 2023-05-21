78bd37e733ab56e52eb653db2bc6e87fc5789d4c
Andreas Bergström <andreas.bergstrom@gmail.com>
2023-05-21 20:59:30 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use] extern crate rocket;
+use rocket::http::Status;
+use rocket::response::{content, status};
+
+#[get("/")]
+fn index() -> status::Custom<content::RawJson<&'static str>> {
+    status::Custom(Status::ImATeapot, content::RawJson("{ \"hello\": \"world\" }"))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
